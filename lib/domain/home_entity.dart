5b46d3c7df8b7da97b448e4d99406b24bf3d339c
Ariana Camargos <arianacamargos16@gmail.com>
2022-06-06 23:09:46 -0300

    feat: adiciona entidade da home page

---
+++ lib/domain/home_entity.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class HomePage extends Equatable {
+  final String title;
+  final String image;
+
+  const HomePage({
+    required this.title,
+    required this.image,
+  });
+
+  @override
+  List<Object?> get props => [
+        title,
+        image,
+      ];
+}
